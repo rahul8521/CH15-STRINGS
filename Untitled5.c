22896be5d2924f07548e7444fdba210244115bc2
rahul8521 <rahulpradhan98756666@gmail.com>
2021-01-19 18:02:46 +0530

    i added this file

---
+++ Untitled5.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <string.h>
+int main( )
+{
+char source[ ] = "Folks!" ;
+char target[ 30 ] = "Hello" ;
+strcat ( target, source ) ;
+printf ( "source string = %s\n", source ) ;
+printf ( "target string = %s\n", target ) ;
+return 0 ;
+}
